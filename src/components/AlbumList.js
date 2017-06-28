48c1c093cf8fc86728f5e2f915a744e3ab6323af
Tetsuro Mikami <tetsuro_mikami@wamazing.jp>
2017-06-29 00:53:22 +0900

    Update state on HTTP request success

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,13 +3,19 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+  // initial state
+  state = { albums: [] };
+
   // get called before this component to be rendered
   componentWillMount() {
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-      .then(response => console.log(response));
+      // always set data via method in `Component`
+      .then(response => this.setState({ albums: response.data }));
   }
 
   render() {
+    console.log(this.state);
+
     return (
       <View>
         <Text>Album List</Text>
