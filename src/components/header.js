7036f603ba88289fbc535e08860cf05ae9bd6e00
Tetsuro Mikami <tetsuro_mikami@wamazing.jp>
2017-06-28 20:41:28 +0900

    Make header text bigger

--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,17 @@ import React from 'react';
 import { Text } from 'react-native';
 
 // create a Component
-const Header = () => (
-  <Text>Albums</Text>
-);
+const Header = () => {
+  const { textStyle } = styles;
+
+  return <Text style={textStyle}>Albums</Text>;
+};
+
+const styles = {
+  textStyle: {
+    fontSize: 20
+  }
+};
 
 // make the Component available to other parts of the App
 export default Header;
