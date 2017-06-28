3493da0759d6bc2022c2294c3d5a88a75c62dbb9
Tetsuro Mikami <tetsuro_mikami@wamazing.jp>
2017-06-28 23:53:46 +0900

    Create AlbumList class

--- index.ios.js
+++ index.ios.js
@@ -2,12 +2,16 @@
 
 // import a library to help create a Component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 // create a Component with JSX
 const App = () => (
-  <Header headerText={'Albums'} />
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
 );
 
 // render it to the device
