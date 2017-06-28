4c158eb0cda082fed2afe47f05b8b2c5f3264e3a
Tetsuro Mikami <tetsuro_mikami@wamazing.jp>
2017-06-29 01:01:45 +0900

    Extract endpoint url to constant

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import axios from 'axios';
 
+const endpoint = 'https://rallycoding.herokuapp.com/api/music_albums';
+
 class AlbumList extends Component {
+
   // initial state
   state = { albums: [] };
 
   // get called before this component to be rendered
   componentWillMount() {
-    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    axios.get(endpoint)
       // always set data via method in `Component`
       .then(response => this.setState({ albums: response.data }));
   }
