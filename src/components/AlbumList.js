0d53e39c916005a8c432bf109c385827e63ae19c
Tetsuro Mikami <tetsuro_mikami@wamazing.jp>
2017-06-29 01:14:57 +0900

    Create AlbumDetail to represent each album

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 const endpoint = 'https://rallycoding.herokuapp.com/api/music_albums';
 
@@ -16,12 +17,17 @@ class AlbumList extends Component {
       .then(response => this.setState({ albums: response.data }));
   }
 
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album} />);
+  }
+
   render() {
     console.log(this.state);
 
     return (
       <View>
-        <Text>Album List</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
