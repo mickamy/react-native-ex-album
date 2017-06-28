02aabbb3e120b29711add31bfc4fb41b85d068b4
Tetsuro Mikami <tetsuro_mikami@wamazing.jp>
2017-06-29 02:21:50 +0900

    Enable scrolling

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
-import AlbumDetail from './AlbumDetail'; 
+import AlbumDetail from './AlbumDetail';
 
 const endpoint = 'https://rallycoding.herokuapp.com/api/music_albums';
 
@@ -26,9 +26,9 @@ class AlbumList extends Component {
     console.log(this.state);
 
     return (
-      <View>
+      <ScrollView>
         {this.renderAlbums()}
-      </View>
+      </ScrollView>
     );
   }
 }
