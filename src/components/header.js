846278c748c3bdf5cbe6413f3be741258a0a9424
Tetsuro Mikami <tetsuro_mikami@wamazing.jp>
2017-06-28 20:57:22 +0900

    Place text center in container

--- src/components/header.js
+++ src/components/header.js
@@ -15,11 +15,20 @@ const Header = () => {
 
 const styles = {
   viewStyle: {
-    backgroundColor: '#F8F8F8'
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative',
   },
   textStyle: {
-    fontSize: 20
-  }
+    fontSize: 20,
+  },
 };
 
 // make the Component available to other parts of the App
