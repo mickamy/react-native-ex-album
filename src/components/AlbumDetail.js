ec00a653d32332bd6fce098c11262b0c6e15e9a3
Tetsuro Mikami <tetsuro_mikami@wamazing.jp>
2017-06-29 02:08:57 +0900

    Show artist thumbnail and other texts

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,16 +1,54 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  const {
+    title,
+    artist,
+    thumbnail_image,
+  } = album;
+
+  const {
+    thumbnailContainerStyle,
+    textContainerStyle,
+    thumbnailStyle,
+  } = styles;
+
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <View style={thumbnailContainerStyle}>
+          <Image
+            style={thumbnailStyle}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={textContainerStyle}>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
       </CardSection>
     </Card>
   );
+};
+
+const styles = {
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  textContainerStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
+  thumbnailStyle: {
+    width: 50,
+    height: 50,
+  }
 };
 
 export default AlbumDetail;
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import axios from 'axios';
-import AlbumDetail from './AlbumDetail';
+import AlbumDetail from './AlbumDetail'; 
 
 const endpoint = 'https://rallycoding.herokuapp.com/api/music_albums';
 
